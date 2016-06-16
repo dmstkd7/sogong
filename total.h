6bcb2b7daa116ef470a9acd8a4f129f3ea7fd3b8
dmstkd7 <eeunsang7@naver.com>
2016-06-16 21:33:38 +0900

    class,collection 등등 임의로 설정하였습니다

--- total.h
+++ total.h
@@ -2,6 +2,21 @@
 #include <string.h>
 using namespace std;
 
+
+/**********************		T			Y			P			E	********************************/
+
+//시간 명령어
+typedef struct Time{
+	int year;
+	int month;
+	int day;
+	int hour;
+}Day;
+
+
+
+/**********************	c	o	l	l	e	c	t	i	o	n	********************************/
+
 //구매 아이템에 대한 컬렉션
 class BidItemCollection{
 private:
@@ -17,6 +32,80 @@ public:
 };
 
 
+class FinishItemCollection{
+private:
+public:
+};
+
+
+/**********************		I		T		E		M	********************************/
+
+
+class Item{
+protected:
+
+	string name;
+	Day	startTime;
+	Day endTime;
+	int totalNum;
+	int startPrice;
+	string ownerId;
+	string itemId;
+
+public:
+	Item();
+	~Item(){
+		cout << "소멸자" << endl;
+	}
+
+};
+
+
+class SellItem : public Item {
+private :
+	Day remainTime;
+
+public:
+	SellItem();
+	~SellItem(){
+		cout << "소멸자" << endl;
+	}
+
+};
+
+class FinishItem : public Item  {
+protected:
+	int totalSoldNumber;
+	int bidderNumber;
+
+public:
+	FinishItem();
+	~FinishItem(){
+		cout << "소멸자" << endl;
+	}
+
+};
+
+
+class BidItem : public Item{
+protected:
+	Day remainTime;
+	int bidPrice;
+
+public:
+	BidItem();
+	~BidItem(){
+		cout << "소멸자" << endl;
+	}
+
+};
+
+
+
+
+
+/**********************		C		L		I		E		T		********************************/
+
 
 //client 구현
 class Client{
@@ -30,7 +119,9 @@ private:
 	string c_email;
 	string c_id;
 	string c_password;
-
+	BidItemCollection bidderItem;
+	SellItemCollection sellerItem;
+	FinishItemCollection finishItem;
 	//비더 , 셀러
 
 public:
@@ -49,4 +140,13 @@ public:
 	void setPassword(string password);
 
 	Client(int session, string name){ c_session = session; c_name = name; }
+
 };
+
+
+
+
+/**********************		전		방		선		언	********************************/
+
+void managementRegisterItem(Client currentUser);
+void registerSellItem(Client currentUser);

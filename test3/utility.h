eb82e74317efe035fdf99a7abbf72c9b01576871
dmstkd7 <eeunsang7@naver.com>
2016-06-20 02:38:33 +0900

    장은상, 최민수 섞여 있습니다. 확인해주세요 장은상 확인 완료

--- test3/utility.h
+++ test3/utility.h
@@ -17,11 +17,80 @@
 #include "Day.h"
 using namespace std;
 
+
+// Function : void querySellItem();
+ // Description: 판매중인 것을 조회하는 함수입니다
+ // Parameters :  없음
+ // Return Value : 없음
+ //
+ // Created: 2016/6/20 02:12 pm
+ // Author: 장은상
 void querySellItem();
+
+// Function : void queryFinishItem();
+ // Description: 판매가 완료된 것을 조회하는 함수입니다
+ // Parameters :  없음
+ // Return Value : 없음
+ //
+ // Created: 2016/6/20 02:18 pm
+ // Author: 장은상
 void queryFinishItem();
+
+// Function : void changeSession();
+ // Description: CurrentUser의 Session을 바꿔주는 함수입니다
+ // Parameters :  없음
+ // Return Value : 없음
+ //
+ // Created: 2016/6/20 02:29 pm
+ // Author: 장은상
 void changeSession();
+
+
+// Function : void managementRegisterItem();
+ // Description: 판매물품등록을 하기 위한 곳으로 가는 함수입니다
+ // Parameters :  없음
+ // Return Value : 없음
+ // Created: 2016/6/20 02:51 pm
+ // Author: 장은상
 void managementRegisterItem();
+
+
+// Function : void setCurrentTime();
+ // Description: 현재 시간을 바꿔주는 함수입니다
+ // Parameters :  없음
+ // Return Value : 없음
+ // Created: 2016/6/20 03:11 pm
+ // Author: 장은상
 void setCurrentTime();
+
+
+// Function : void managementBIdItem(Client currentUser, BidItemCollection BidItemCollect, SellItemCollection SellItemCollect);
+ // Description: 구매물품등록을 하기 위한 곳으로 가는 함수입니다
+ // Parameters :  Client currentUser - 현재 유저의 정보를 담은 클래스
+ //               BidItemCollection BidItemCollect - 현재 존재하는 BidItemCollection
+ //               SellItemCollection SellItemCollect - 현재 존재하는 SellitemCollection
+ // Return Value : 없음
+ // Created: 2016/6/19 02:11 pm
+ // Author: 최민수
 void managementBIdItem(Client currentUser, BidItemCollection BidItemCollect, SellItemCollection SellItemCollect);
+
+
+// Function :void queryBidItem(Client currentUser, BidItemCollection BidItemCollect, SellItemCollection SellItemCollect);
+ // Description: 구매물품등록을 하기 위한 곳으로 가는 함수입니다
+ // Parameters :  Client currentUser - 현재 유저의 정보를 담은 클래스
+ //               BidItemCollection BidItemCollect - 현재 존재하는 BidItemCollection
+ //               SellItemCollection SellItemCollect - 현재 존재하는 SellitemCollection
+ // Return Value : 없음
+ // Created: 2016/6/18 16:20 pm
+ // Author: 최민수
 void queryBidItem(Client currentUser, BidItemCollection BidItemCollect, SellItemCollection SellItemCollect);
+
+
+// Function :void queryMyBidItem(Client currentUser, BidItemCollection BidItemCollect);
+ // Description: 구매물품등록을 하기 위한 곳으로 가는 함수입니다
+ // Parameters :  Client currentUser - 현재 유저의 정보를 담은 클래스
+ //               BidItemCollection BidItemCollect - 현재 존재하는 BidItemCollection
+ // Return Value : 없음
+ // Created: 2016/6/18 23:20 pm
+ // Author: 최민수
 void queryMyBidItem(Client currentUser, BidItemCollection BidItemCollect);

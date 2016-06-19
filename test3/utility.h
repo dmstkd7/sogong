2faeb1f2d4ed5671b4daae53bb05bea125668e9d
dmstkd7 <eeunsang7@naver.com>
2016-06-20 02:18:43 +0900

    장은상 완료

--- test3/utility.h
+++ test3/utility.h
@@ -1,3 +1,14 @@
+// Class : utility.h
+ // Description: 메인에서 쓸 함수를 한 데 모아둔 것입니다
+ // Created: 2016/6/20 02:11 pm
+ // Author: 장은상
+ // mail: eeunsang7@naver.com
+ //
+ // Revsions : 
+ //   1. When & Who : 22016/6/20 02:11 pm 02:30 pm by EunsangJang
+ //      What : 팀원과 합치는 과정에서 queryBidItem, queryMyBidItem, managemnetRegisterItem()등을 추가하였습니다
+
+
 #pragma once
 #include "Client.h"
 #include "BidItemCollection.h"
